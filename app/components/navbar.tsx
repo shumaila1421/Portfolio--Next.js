ebeedbb66df8e24464b1988d53baafc9708a47b3
Shumaila Riaz <shumailariaz215@gmail.com>
2026-03-12 21:46:57 +0500

    new links added

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -9,7 +9,7 @@ const navLinks = [
   { name: "About", href: "#about" },
   { name: "Skills", href: "#skills" },
   { name: "Projects", href: "#projects" },
-  // { name: "Contact", href: "#contact" },
+  { name: "Contact", href: "#contact" },
 ];
 
 export default function Navbar() {
